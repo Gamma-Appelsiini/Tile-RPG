e63b3f2a74b59ac1e09a5684b23c5331dc3c3402
Gamma-Appelsiini <paulitorma@hotmail.com>
2025-08-04 12:47:47 +0300

    Update player_camera.gd
    Finished camera movement

--- Camera/player_camera.gd
+++ Camera/player_camera.gd
@@ -1,9 +1,58 @@
 extends Camera3D
+class_name PlayerCamera
 
 @export var pivot:Node3D
+var input_enabled:bool = true
+
+var rotating:bool = false
+var zoom_enabled:bool = true
+const ZOOM_DEFAULT:float = 11
+const RESET_TIME:float = 0.75
+var old_zoom:float = 0
 
 func _input(event: InputEvent) -> void:
+	if !input_enabled: return
 	if event.is_action_pressed("Rotate_Cam_L"):
-		pivot.rotate_y(deg_to_rad(-90))
+		_rotate_cam(-90)
 	elif event.is_action_pressed("Rotate_Cam_R"):
-		pivot.rotate_y(deg_to_rad(90))
+		_rotate_cam(90)
+	elif event.is_action_pressed("roll_up"):
+		_zoom_cam(-1)
+	elif event.is_action_pressed("roll_down"):
+		_zoom_cam(1)
+
+func _rotate_cam(amount:float) -> void:
+	if rotating: return
+	rotating = true
+	
+	var rotation_time:float = 0.25
+	var tween:Tween = create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(pivot,"rotation:y",pivot.rotation.y + deg_to_rad(amount), rotation_time)
+	
+	await tween.finished
+	rotating = false
+
+func _zoom_cam(dir:float) -> void:
+	if !zoom_enabled: return
+	const MAX_ZOOM_IN:float = 4.5
+	const MAX_ZOOM_OUT:float = 20
+	
+	var move_amount: float = 0.5
+	self.size = clamp(self.size + move_amount * dir, MAX_ZOOM_IN, MAX_ZOOM_OUT)
+
+func disable_zooming() -> void:
+	zoom_enabled = false
+	old_zoom = self.size
+	
+	var tween:Tween = create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(self,"size",ZOOM_DEFAULT, RESET_TIME)
+	
+func enable_zooming() -> void:
+	var tween:Tween = create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(self,"size",old_zoom, RESET_TIME)
+	
+	await tween.finished
+	zoom_enabled = true
