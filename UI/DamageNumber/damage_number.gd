13b91ade64bf9d00b3037f7f253f40e767f6533c
Gamma-Appelsiini <paulitorma@hotmail.com>
2025-10-07 13:10:44 +0300

    Improved damage number visuals

--- UI/DamageNumber/damage_number.gd
+++ UI/DamageNumber/damage_number.gd
@@ -3,8 +3,9 @@ class_name DamageNumber
 
 @export var number_label:Label = null
 
-const DURATION:float = 2
+const DURATION:float = 1.5
 
+var offset:Vector3 = Vector3(0.5,1.5,0.5)
 var game_camera:Camera3D = null
 var position_node:Node3D = null
 
@@ -12,21 +13,28 @@ func _ready() -> void:
 	set_process(false)
 	number_label.scale = Vector2(0,0)
 
-func spawn_at_node(number:int, target_pos:Node3D) -> void:
+func spawn_at_node(number:int, target_pos:Node3D, crit:bool = false) -> void:
 	self.position_node = target_pos
 	game_camera = GlobalSignals.player.player_camera
 	number_label.text = str(number)
 	
+	offset = Vector3(randf_range(-0.2,0.2),randf_range(1.45,1.85),randf_range(-0.2,0.2))
 	set_process(true)
+	if crit: _handle_crit()
 	_animate_label()
 
+func _handle_crit() -> void:
+	number_label.add_theme_color_override("font_outline_color", Color(1.0, 1.0, 1.0, 1.0))
+	number_label.add_theme_color_override("font_color", Color(0.0, 0.0, 0.0, 1.0))
+	number_label.add_theme_font_size_override("font_size", 62)
+
 func _process(_delta: float) -> void:
-	var screen_position:Vector2 = game_camera.unproject_position(position_node.global_transform.origin)
+	var screen_position:Vector2 = game_camera.unproject_position(position_node.global_transform.origin + offset)
 	self.global_position = screen_position
 
 func _animate_label() -> void:
-	var tween2:Tween = create_tween().set_ease(Tween.EASE_OUT_IN)
-	tween2.tween_property(number_label,"position", number_label.position + Vector2(1,1), DURATION)
+	var tween2:Tween = create_tween().set_ease(Tween.EASE_OUT)
+	tween2.tween_property(number_label,"position", number_label.position + Vector2(0,-150), DURATION)
 	
 	var tween:Tween = create_tween().set_ease(Tween.EASE_OUT)
 	tween.tween_property(number_label,"scale",Vector2(1.5,1.5), 0.2)
@@ -37,7 +45,7 @@ func _animate_label() -> void:
 	await tween2.finished
 	
 	tween = create_tween().set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(number_label,"scale",Vector2(0,0), 0.1)
+	tween.tween_property(number_label,"modulate:a",0, 0.2)
 	await tween.finished
 	
 	queue_free()
--- UI/ui_handler.gd
+++ UI/ui_handler.gd
@@ -6,6 +6,8 @@ class_name UIHandler
 @export var stat_window: StatWindow = null
 @export var dialogue_window: DialogueWindow = null
 
+const DAMAGE_NUMBER_SCENE:PackedScene = preload("uid://dac2s2r20qif4")
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Character"):
 		stat_window.visible = !stat_window.visible
@@ -17,3 +19,8 @@ func set_player(player:Player) -> void:
 	inventory.set_player(player)
 	dialogue_window.set_player(player)
 	stat_window.set_game_character(player)
+
+func show_damage_number(target_node:Node3D, amount:int, crit:bool = false) -> void:
+	var new_number:DamageNumber = DAMAGE_NUMBER_SCENE.instantiate()
+	add_child(new_number)
+	new_number.spawn_at_node(amount,target_node, crit)
