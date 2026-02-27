96099b33621db3a87bd2bfc2a8fd1bdbfb544dba
Gamma-Appelsiini <paulitorma@hotmail.com>
2026-02-27 15:50:33 +0200

    Fixed shop button. Added item generating for shop and buying

--- UI/Shop/shop_window.gd
+++ UI/Shop/shop_window.gd
@@ -2,11 +2,27 @@ extends PanelContainer
 class_name ShopWindow
 
 const SHOP_ITEM_PANEL := preload("uid://dq3yj14tiaaho")
+const ITEM_AMOUNT:int = 8
+const RARITIES:Array[Item.ItemRarity] = [Item.ItemRarity.POOR,Item.ItemRarity.POOR,
+	Item.ItemRarity.COMMON,Item.ItemRarity.COMMON, Item.ItemRarity.RARE, Item.ItemRarity.RARE, Item.ItemRarity.EPIC, Item.ItemRarity.LEGENDARY]
 
-@onready var shop_items_container: GridContainer = $ShopItemsContainer
+@export var shop_items_container: GridContainer = null
+@export var purchase_sound:AudioStream = null
 
 func _ready() -> void:
-	pass
+	_fill_items()
 
 func _fill_items() -> void:
-	pass
+	for i:int in ITEM_AMOUNT:
+		var new_item_panel:ShopItemPanel = SHOP_ITEM_PANEL.instantiate()
+		shop_items_container.add_child(new_item_panel)
+		var max_item_level:int = GlobalSignals.player.stat_handler.get_stat_amount(Stats.CharStat.CURRENT_LEVEL)
+		new_item_panel.add_item(RARITIES[i], max_item_level)
+		new_item_panel.default_button.purchased.connect(_buy_item.bind(new_item_panel))
+
+func _buy_item(item_panel:ShopItemPanel) -> void:
+	var player_cash:int = 0
+	if item_panel.default_button.item_price > player_cash: return
+	
+	GlobalSignals.play_audio.emit(purchase_sound, AudioManager.AUDIO_TYPE.UI)
+	item_panel.default_button.set_as_sold()
--- UI/generic_button.gd
+++ UI/generic_button.gd
@@ -1,6 +1,8 @@
 extends PanelContainer
 class_name DefaultButton
 
+signal purchased
+
 @export var button_texture_rect: TextureRect = null
 @export var glow: ColorRect = null
 @export var button_press_sound:AudioStream = null
@@ -13,18 +15,31 @@ const COST_LABEL_THEME := preload("uid://crj6kfsca2qxw")
 
 var mouse_pressed:bool = false
 
+func set_price(amount:int) -> void:
+	price_label.text = str(amount)
+
+func set_as_sold() -> void:
+	set_process_input(false)
+	price_label.add_theme_color_override("font_color", Color(0.79, 0.095, 0.303, 1.0))
+	price_label.text = "Sold"
+	button_texture_rect.mouse_entered.disconnect(_on_m_enter)
+	button_texture_rect.mouse_exited.disconnect(_on_m_leave)
+
 func _ready() -> void:
+	set_process_input(false)
 	_connect_signals()
 	
+func _on_m_enter() -> void:
+	glow.visible = true
+	set_process_input(true)
+	
+func _on_m_leave() -> void:
+	glow.visible = false
+	if mouse_pressed == false: set_process_input(false)
+	
 func _connect_signals() -> void:
-	button_texture_rect.mouse_entered.connect(func():
-		glow.visible = true
-		set_process_input(true)
-		)
-	button_texture_rect.mouse_exited.connect(func():
-		glow.visible = false
-		if mouse_pressed == false: set_process(false)
-		)
+	button_texture_rect.mouse_entered.connect(_on_m_enter)
+	button_texture_rect.mouse_exited.connect(_on_m_leave)
 
 func _set_text_override() -> void:
 	price_label.add_theme_font_size_override("font_size", 28)
@@ -34,15 +49,21 @@ func _remove_text_override() -> void:
 	price_label.self_modulate = Color("ffffff")
 	price_label.remove_theme_font_size_override("font_size")
 
+func _on_mouse_release() -> void:
+	mouse_pressed = false
+	button_texture_rect.texture = BUTTON_DEFAULT
+	_remove_text_override()
+	GlobalSignals.play_audio.emit(button_release_sound, AudioManager.AUDIO_TYPE.UI)
+	
+	if !glow.visible: set_process_input(false)
+	else: purchased.emit()
+
+func _on_mouse_press() -> void:
+	mouse_pressed = true
+	button_texture_rect.texture = BUTTON_PRESSED
+	_set_text_override()
+	GlobalSignals.play_audio.emit(button_press_sound, AudioManager.AUDIO_TYPE.UI)
+
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("Left Click"):
-		mouse_pressed = true
-		button_texture_rect.texture = BUTTON_PRESSED
-		_set_text_override()
-		GlobalSignals.play_audio.emit(button_press_sound, AudioManager.AUDIO_TYPE.UI)
-	elif event.is_action_released("Left Click"):
-		mouse_pressed = false
-		button_texture_rect.texture = BUTTON_DEFAULT
-		_remove_text_override()
-		GlobalSignals.play_audio.emit(button_release_sound, AudioManager.AUDIO_TYPE.UI)
-		if !glow.visible: set_process(false)
+	if event.is_action_pressed("Left Click"): _on_mouse_press()
+	elif event.is_action_released("Left Click"): _on_mouse_release()
