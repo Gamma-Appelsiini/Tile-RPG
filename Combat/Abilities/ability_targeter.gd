77a863d748feb14ca522e881bd26d8db739622d6
Gamma-Appelsiini <paulitorma@hotmail.com>
2026-04-28 16:54:27 +0300

    Updated range visual

--- Combat/Abilities/ability_targeter.gd
+++ Combat/Abilities/ability_targeter.gd
@@ -11,6 +11,7 @@ const NON_CANCELLING_ACTIONS: Array[StringName] = [
 	&"ability 4", &"ability 5", &"ability 6", &"ability 7", 
 	&"ability 8", &"ability 9",
 ]
+const RANGE_MESH_MATERIAL := preload("uid://ckgonjkqks5ra")
 
 var player:Player = null
 var player_camera:Camera3D = null
@@ -67,6 +68,11 @@ func _process(_delta: float) -> void:
 	_get_ability_target()
 
 func _hide_range_indicators() -> void:
+	range_mesh.hide()
+	range_mesh.queue_free()
+	range_mesh = null
+	return
+	
 	for ind:RangeIndicator in range_indicators:
 		ind._reset_color()
 		ind.hide()
@@ -75,6 +81,10 @@ func _visualize_tiles_in_range() -> void:
 	var player_tile:Tile = tile_manager.char_tiles[player]
 	var tiles_in_range:Array[Tile] = tile_manager.get_tiles_in_range(player_tile, selected_ability.get_range())
 	_add_range_tiles(len(tiles_in_range))
+	
+	tiles_in_range.push_back(player_tile)
+	range_mesh = _create_array_mesh(tiles_in_range, player_tile)
+	return
 	
 	for i:int in len(tiles_in_range):
 		var indicator:RangeIndicator = range_indicators[i]
@@ -153,6 +163,91 @@ func _set_new_target_tile(new_target:Tile) -> void:
 func _hide_aoe() -> void:
 	for aoe_ind:Node3D in aoe_indicators:
 		aoe_ind.visible = false
+
+# Create an array mesh based on the tiles.
+# Origin of mesh should be start tile. Start tile is in tiles array.
+# Tiles are 1x1 meter squares that have global_position Vector3.
+func _create_array_mesh(tiles: Array[Tile], start_tile: Tile) -> MeshInstance3D:
+	var new_mesh: MeshInstance3D = MeshInstance3D.new()
+	var new_array_mesh: ArrayMesh = ArrayMesh.new()
+
+	if tiles.is_empty() or not is_instance_valid(start_tile):
+		return new_mesh
+
+	# 1. Map the grid to easily find neighbors
+	var grid := {}
+	for tile in tiles:
+		var local = tile.global_position - start_tile.global_position
+		grid[Vector2(round(local.x), round(local.z))] = true
+
+	var vertices := PackedVector3Array()
+	var normals := PackedVector3Array()
+	var uvs := PackedVector2Array()
+	var colors := PackedColorArray()
+	var indices := PackedInt32Array()
+
+	for tile in tiles:
+		var offset = vertices.size()
+		var local = tile.global_position - start_tile.global_position
+		var pos = Vector2(round(local.x), round(local.z))
+
+		# 2. Check 8 surrounding spaces (1 = Empty Space / Outer Boundary, 0 = Solid Neighbor)
+		var mask: int = 0
+		if not grid.has(pos + Vector2(0, -1)): mask |= 1    # Top
+		if not grid.has(pos + Vector2(1, 0)):  mask |= 2    # Right
+		if not grid.has(pos + Vector2(0, 1)):  mask |= 4    # Bottom
+		if not grid.has(pos + Vector2(-1, 0)): mask |= 8    # Left
+		if not grid.has(pos + Vector2(-1, -1)): mask |= 16  # Top-Left Corner
+		if not grid.has(pos + Vector2(1, -1)):  mask |= 32  # Top-Right Corner
+		if not grid.has(pos + Vector2(1, 1)):   mask |= 64  # Bottom-Right Corner
+		if not grid.has(pos + Vector2(-1, 1)):  mask |= 128 # Bottom-Left Corner
+
+		# 3. Pack the mask into a float (0.0 to 1.0) to send to the shader
+		var color_val = float(mask) / 255.0
+		var tile_color = Color(color_val, 0.0, 0.0, 1.0)
+
+		# 4. Build the Quad
+		vertices.append_array([
+			local + Vector3(-0.5, 0, -0.5), # TL
+			local + Vector3(-0.5, 0, 0.5),  # BL
+			local + Vector3(0.5, 0, 0.5),   # BR
+			local + Vector3(0.5, 0, -0.5)   # TR
+		])
+
+		normals.append_array([Vector3.UP, Vector3.UP, Vector3.UP, Vector3.UP])
+		uvs.append_array([Vector2(0, 0), Vector2(0, 1), Vector2(1, 1), Vector2(1, 0)])
+		colors.append_array([tile_color, tile_color, tile_color, tile_color])
+
+		indices.append_array([
+			offset + 0, offset + 3, offset + 2, 
+			offset + 0, offset + 2, offset + 1
+		])
+
+	var arrays := []
+	arrays.resize(Mesh.ARRAY_MAX)
+	arrays[Mesh.ARRAY_VERTEX] = vertices
+	arrays[Mesh.ARRAY_NORMAL] = normals
+	arrays[Mesh.ARRAY_TEX_UV] = uvs
+	arrays[Mesh.ARRAY_COLOR] = colors
+	arrays[Mesh.ARRAY_INDEX] = indices
+
+	new_array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	new_mesh.mesh = new_array_mesh
+
+	add_child(new_mesh)
+	new_mesh.global_position = start_tile.global_position + Vector3(0,0.05,0)
+	new_mesh.material_override = RANGE_MESH_MATERIAL
+
+	return new_mesh
+
+# Helper math function: Shortest distance from a point to a line segment
+func _dist_to_segment(p: Vector2, v: Vector2, w: Vector2) -> float:
+	var length_squared = v.distance_squared_to(w)
+	if length_squared == 0.0: 
+		return p.distance_to(v)
+	var t = max(0.0, min(1.0, (p - v).dot(w - v) / length_squared))
+	var projection = v + t * (w - v)
+	return p.distance_to(projection)
 
 func _show_aoe() -> void:
 	var tiles_in_aoe:Array[Tile] = selected_ability.get_tiles_in_aoe(hovered_tile)
--- Interactables/anvil.gd
+++ Interactables/anvil.gd
@@ -12,13 +12,6 @@ func interact() -> void:
 	GlobalSignals.open_crafting.emit()
 	ui_handler.crafting_window.visibility_changed.connect(_crafting_closed, CONNECT_ONE_SHOT)
 
-func _add_crafted_item_back_to_inv() -> void:
-	pass
-	#if !crafting_window.crafting_equipment: return
-	
-	#var added:bool = player_inv.add_item_to_inv(crafting_window.crafting_equipment)
-	#if added: crafting_window.clear_equ()
-
 func _crafting_closed() -> void:
 	if ui_handler.crafting_window.visible:
 		ui_handler.crafting_window.visibility_changed.connect(_crafting_closed, CONNECT_ONE_SHOT)
